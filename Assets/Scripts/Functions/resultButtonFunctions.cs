b586ab87ed1b19e3a232dc066c45ecbbb14f9cce
MK\owner <motomoto919919@gmail.com>
2021-03-11 21:18:48 +0900

    resultに「タイトルへ戻る」、「コンティニュー」ボタンを追加

---
+++ Assets/Scripts/Functions/resultButtonFunctions.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class resultButtonFunctions : MonoBehaviour
+{
+    public Image Image;
+    public AudioSource OnButton, Click;
+    float fadeSpeed = 0.02f;
+    float r, g, b, a;
+    private void Awake()
+    {
+        r = Image.color.r;
+        g = Image.color.g;
+        b = Image.color.b;
+        a = Image.color.a;
+    }
+    public void ClickContinue()
+    {
+        Click.Play();
+        StartCoroutine(SceneChenge(0));
+    }
+    public void ClickReturn()
+    {
+        Click.Play();
+        StartCoroutine(SceneChenge(1));
+    }
+    public void PointerEnter()
+    {
+        OnButton.Play();
+    }
+    private IEnumerator SceneChenge(int num)
+    {
+        Image.enabled = true;
+        while (a < 1)
+        {
+            a += fadeSpeed;
+            yield return null;
+            Image.color = new Color(r, g, b, a);
+        }
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(num);
+    }
+}
